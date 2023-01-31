2ab60995c116b712eca35d28d4550419bbb7f77b
annaduraij <122567409+annaduraij@users.noreply.github.com>
2023-01-31 16:23:49 -0500

    Implemented Color Fade into Ex2_2
    Utilized lerpColor to create a fading effect. Solved the issues of sudden transitions and choppiness from the previous solution of limiting FPS.

--- Exercises/Exercise 2/js/ex2_2.js
+++ Exercises/Exercise 2/js/ex2_2.js
@@ -10,7 +10,7 @@ let circleX,circleY,circleR;
 
 //Color Vars that will be respectively the input and output of the removeColorRed function that is the highlight of the exercise
 //Will assigned with a 'color(r,g,b)' value in setup()
-let colorIn,colorOut;
+let colorRandom,colorIn,colorOut;
 
 
 //------------------------------------------------------------
@@ -28,8 +28,13 @@ function setup(){
     //Set a nominal radius value that somewhat scales off of the user's window size
     circleR = 1/8 * canvasH;
 
+    //Initialize colorIn
+    colorIn = color(0,0,0);
+    //Initialize colorRandom
+    colorRandom = color(0,0,0);
+
     //Set FrameRate to 5 as 60 strobes the color
-    frameRate(4);
+    frameRate(60);
 
 }
 
@@ -41,19 +46,54 @@ function draw(){
     //Clear the Canvas of the Previous Circle
     clear();
 
-    //Initialize colorIn with a random RGB value
-    colorIn = color(random(0,255), random(0,255), random(0,255));
+    //Log the Color In value
+    //console.log("Color In:",colorIn.toString());
 
-    //Log the value
-    console.log("Color In:",colorIn);
+    //Unassemble the Color Objects by Parsing Them
+    colorIn = parseColor(colorIn)
+    colorRandom = parseColor(colorRandom);
+
+    /*Due to the Nature of the Color Interpolation Rounding being slightly off
+        Must use a series of comparisons between the parsed color values of the ColorIn and ColorRandom
+        As long as it's roughly the same, I chose a tolerance level of 2
+        Assume the colorIn has reached the colorRandom and it's time to assign a new Random Color
+     */
+    const colorTolerance = 10;
+    //Once ColorIn Matches the Random Color, reset the Random Color
+    if(
+        (Math.abs(colorIn[0] - colorRandom[0]) <= colorTolerance)
+        &&
+        (Math.abs(colorIn[1] - colorRandom[1]) <= colorTolerance)
+        &&
+        (Math.abs(colorIn[2] - colorRandom[2]) <= colorTolerance)
+    ){
+        colorRandom = parseColor(color(random(0,255), random(0,255), random(0,255)));
+        //Log the New Random Color Value
+        console.log("Random Color:",colorRandom.toString());
+    }
+
+    //Reassemble the parsed Colors into p5 Color Objects
+    colorIn = color(colorIn[0],colorIn[1],colorIn[2])
+    colorRandom = color(colorRandom[0],colorRandom[1],colorRandom[2])
+
+    //Slowly fade colorIn to colorRandom with increments of 5% using the lerpColor function
+    let interpolatedColor = lerpColor(colorIn, colorRandom, 0.05);
+    //Log the Interpolated Color
+    //console.log("Interpolated Color:",interpolatedColor);
+
+    //Parse and Pass the Interpolated Color to ColorIn
+    interpolatedColor = parseColor(interpolatedColor);
+    colorIn = color(interpolatedColor[0],interpolatedColor[1],interpolatedColor[2]);
+
+
 
     //Assign colorOut to the array output of 'removeColorRed(colorIn)
-    colorOut = removeColorRed(colorIn);
+    colorOut = removeColorRed(parseColor(colorIn));
     //Use p5.js inline to parse the array's color values as a color
     colorOut = color(colorOut[0],colorOut[1],colorOut[2])
 
-    //Log the value
-    console.log("Color Out:",colorOut);
+    //Log the Final Color Out value
+    //console.log("Color Out:",colorOut.toString());
 
 
 
@@ -79,11 +119,11 @@ function draw(){
 //                     P5 User Functions
 //------------------------------------------------------------
 
-//Function that removes the red color value from an input color
+//Function that returns a parsed [r,g,b,a] array from a p5.js color
 //Argument must be a p5.js rgb color fed in the form of 'color(r,g,b)'
-function removeColorRed(color){
+function parseColor(colorObj){
     //Convert the Color Object into a String
-    let colorStr = color.toString();
+    let colorStr = colorObj.toString();
 
     //Trim the 'rgba(' portion by splitting on '(' and grabbing the string after it
     colorStr = colorStr.split("(")
@@ -100,11 +140,28 @@ function removeColorRed(color){
     let r = colorStr[0];
     let g = colorStr[1];
     let b = colorStr[2];
+    let a = colorStr[3];
+
+    //Return the Color Values
+    return [r, g, b, a];
+
+}// End of removeColorRed function
+
+//Function that removes the red color value from an input color
+//Argument must be a p5.js rgb color fed in the form of 'color(r,g,b)'
+function removeColorRed(colorArray){
+
+    //The array should correspond to [r,g,b,a]
+    //Grab the values and store as r, g, b values
+    let r = colorArray[0];
+    let g = colorArray[1];
+    let b = colorArray[2];
+    let a = colorArray[3];
 
     //Set red component to zero
     r = 0;
 
-    //Return the Color Values
-    return [r, g, b];
+    //Return the Color Values and Alpha Value
+    return [r, g, b, a];
 
 }// End of removeColorRed function
