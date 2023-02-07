bbb31a1dccb93cd4987b9a429c0d63aef33db32f
annaduraij <122567409+annaduraij@users.noreply.github.com>
2023-02-07 17:36:37 -0500

    Completed Ex4_1.js
    Created inner Style Object for Obj Literal as it turns out .attribute.width is not for divs.
    
    Good news, found that you can use the window object to call methods, with the caveat it only works on global objects.
    
    Modified code to reflect shifting style properties to a style inner object and the calling of style methods with global Window Object in array format rather than typical method format.

--- Exercises/Exercise 4/js/ex4_1.js
+++ Exercises/Exercise 4/js/ex4_1.js
@@ -3,25 +3,27 @@
 //                   Variable Declarations
 //------------------------------------------------------------
 //Create Obj Literal for Square Div
-    //Properties of 'divObj':
-        //tag: 'div'
-        //attributes: {id: 'squareDiv', width: '100px', height: '100px', backgroundColor: 'green'}
-
 //Representation of an HTML Object as a JS
-divObj = {
+let divObj = {
     //HTML Tag
     tag:'div',
     //HTML Attributes as internal object
     attributes: {
         //Object DOM ID
         id: 'squareDiv',
+    },//End of inner Attributes Object
+    style: {
         //Object Size
         width: '100px',
         height: '100px',
         //Object Background Color
         backgroundColor: 'green'
-    }//End of inner Attributes Object
+    },//End of inner Style Object
+
 }//End of JS Object Literal to represent HTML
+
+//Global Element Variable Such that the Window Object Can be Used
+var element = 1;
 
 
 //------------------------------------------------------------
@@ -29,8 +31,9 @@ divObj = {
 //------------------------------------------------------------
 
 //Append Element as Child Node to HTML Body
-    //Get and bind HTML Body Element using
-    const pageBody = document.getElementsByTagName('body');
+    //Get and bind HTML Body Element using get Elements by Tag Name
+        //Note this Returns an Array, so just get the first Element aka [0]
+    const pageBody = document.getElementsByTagName('body')[0];
 
     //Build Child Node using buildHTML function and then Append to HTML Body
     pageBody.appendChild(buildHTML(divObj));
@@ -55,21 +58,44 @@ divObj = {
     function buildHTML(elementObj)
     {
         //Create HTML Element and Implant into Carrier Variable
-        let element = document.createElement(elementObj.tag);
+        element = document.createElement(elementObj.tag);
+
+        console.log("HTML Element", element);
+        console.log("Window: HTML Element", window['element']);
 
         //Attach Attributes to the HTML Div using a for...in... loop [Equivalent of forEach in Associative Arrays]
         for (let attribute in elementObj.attributes){
 
-            //Add a If-Else Statement to Catch a CamelCase to KebabCase situation
-            if (attribute == "backgroundColor") {
-                element.setAttribute(background-color,elementObj.attributes[attribute]);
-            }
             //Set Element Attribute
-            else {
-                element.setAttribute(attribute,divObj.attributes[attribute]);
-            }
+            element.setAttribute(attribute,divObj.attributes[attribute]);
 
         } //End of forIn Loop
+
+        //Attach Style Properties to the HTML Div using a for...in... loop [Equivalent of forEach in Associative Arrays]
+        for (let property in elementObj.style){
+
+            //Use Global Window Parent Object:
+            //https://stackoverflow.com/questions/359788/how-to-execute-a-javascript-function-when-i-have-its-name-as-a-string
+
+            /*
+            //Add a If-Else Statement to Catch a CamelCase to KebabCase situation
+            if (elementObj.style[property] === "backgroundColor") {
+                //Overwrite
+                window['element']['style']['background-color'] = elementObj.style[property];
+            } */
+            //Set Element Attribute
+            //else {
+            console.log(property,elementObj.style[property]);
+
+            //Use the Window Object to Call the 'Object.style.property' Method
+            window['element']['style'][property.toString()] = elementObj.style[property];
+
+
+
+            //}
+
+        } //End of forIn Loop
+
 
         //Return the Completed HTML Object
         return element;
@@ -84,23 +110,23 @@ divObj = {
         //Split 'px' Unit from JS Object's Height and Width Attributes
             //Split String into Array of Segmented Strings
             //Retain only the first part, [0], which should contain the number
-        elementObj.attributes.width = elementObj.attributes.width.split('px')[0];
-        elementObj.attributes.height = elementObj.attributes.height.split('px')[0];
+        elementObj.style.width = elementObj.style.width.split('px')[0];
+        elementObj.style.height = elementObj.style.height.split('px')[0];
 
         //Scale existing JS Object's Height and Width by 10%
-        elementObj.attributes.width *= 1.1;
-        elementObj.attributes.height *= 1.1;
+        elementObj.style.width *= 1.1;
+        elementObj.style.height *= 1.1;
 
         //Append 'px' unit back on to the end of JS Object's Height and Width Attributes
-        elementObj.attributes.width += 'px';
-        elementObj.attributes.height += 'px';
+        elementObj.style.width += 'px';
+        elementObj.style.height += 'px';
 
         //Get the Document Element with ID
         let docElement = document.getElementById(elementObj.attributes.id);
 
         //Set the Document Elements' New Size Attributes
-        docElement.setAttribute('width', elementObj.attributes.width);
-        docElement.setAttribute('height', elementObj.attributes.width);
+        docElement.style.width = elementObj.style.width;
+        docElement.style.height = elementObj.style.height;
 
     } //End of function scale()
 
