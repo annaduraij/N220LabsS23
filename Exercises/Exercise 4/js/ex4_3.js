0218a0f1a89694fb06ad0e0f9cd94fa7d641611d
annaduraij <122567409+annaduraij@users.noreply.github.com>
2023-02-13 20:08:40 -0500

    Completed Ex4_3.js
    Completed Ex4_3.js
    
    Will be updated to true OOP Approach!

--- Exercises/Exercise 4/js/ex4_3.js
+++ Exercises/Exercise 4/js/ex4_3.js
@@ -7,31 +7,56 @@
         //tag: 'div'
         //attributes: {id: 'squareDiv', width: '100px', height: '100px', backgroundColor: 'blue'}
 
+//Representation of an HTML Object as a JS Object
+let divObj = {
+    //HTML Tag
+    tag:'div',
+    //HTML Attributes as internal object
+    attributes: {
+        //Object DOM ID
+        id: 'squareDiv',
+    },//End of inner Attributes Object
+    style: {
+        //Object Size
+        width: '100px',
+        height: '100px',
+        //Object Background Color
+        backgroundColor: 'blue'
+    },//End of inner Style Object
+
+}//End of JS Object Literal to represent HTML
+
+//Global ElementHTML Variable
+//Declared Here so that it can be referenced by the Window Object in the BuildHTML function
+var elementHTML = 1;
 
 //------------------------------------------------------------
 //                        General JS
 //------------------------------------------------------------
 //Append Element as Child Node to HTML Body
-    //Get and bind HTML Body Element using
-    const pageBody = document.getElementsByTagName('body');
-
+    //Get and bind HTML Body Element using get Elements by Tag Name
+        //Note this Returns an Array, so just get the first Element aka [0]
+    const pageBody = document.getElementsByTagName('body')[0];
+    
     //Build Child Node using buildHTML function and then Append to HTML Body
     pageBody.appendChild(buildHTML(divObj));
 
 //Build Event Listener for HTML Onclick
     //Acquire Element via ID
-    let divHTML = document.getElementById(divObj.attributes.id);
+    elementHTML = document.getElementById(divObj.attributes.id);
 
 //Event Listeners and Functions such that the Div is Black during onMouseOver and Blue during onMouseOut
     //Attach 'onmouseover' Event Listener that runs the changeColor Function
         //Use an Anonymous Function for the changeColor with Arguments of divObj,'black'
-    divHTML.addEventListener('onmouseover', function () {
+    elementHTML.addEventListener('mouseover', function () {
+        console.log("Mouse Over!");
         changeColor (divObj,'black');
     });//End of Anonymous Function
 
     //Attach 'onmouseout' Event Listener that runs the changeColor Function
         //Use an Anonymous Function for the changeColor with Arguments of divObj,'blue'
-    divHTML.addEventListener('onmouseout', function () {
+    elementHTML.addEventListener('mouseout', function () {
+        console.log("Mouse Out!");
         changeColor (divObj,'blue');
     });//End of Anonymous Function
 
@@ -39,25 +64,34 @@
 //------------------------------------------------------------
 //                       JS Functions
 //------------------------------------------------------------
-//Build HTML Element from JS
+// Build HTML Element from JS Object
 function buildHTML(elementObj)
 {
     //Create HTML Element and Implant into Carrier Variable
-    let elementHTML = document.createElement(elementObj.tag);
+    elementHTML = document.createElement(elementObj.tag);
 
     //Attach Attributes to the HTML Div using a for...in... loop [Equivalent of forEach in Associative Arrays]
     for (let attribute in elementObj.attributes){
 
-        //Add a If-Else Statement to Catch a CamelCase to KebabCase situation
-        if (attribute == "backgroundColor") {
-            elementHTML.setAttribute(background-color,elementObj.attributes[attribute]);
-        }
-        //Set Element Attribute
-        else {
-            elementHTML.setAttribute(attribute,divObj.attributes[attribute]);
-        }
+        //Set elementHTML Attribute
+        elementHTML.setAttribute(attribute,divObj.attributes[attribute]);
 
     } //End of forIn Loop
+
+    //Attach Style Properties to the HTML Div using a for...in... loop [Equivalent of forEach in Associative Arrays]
+    for (let property in elementObj.style){
+
+        //Use Global Window Parent Object:
+        //https://stackoverflow.com/questions/359788/how-to-execute-a-javascript-function-when-i-have-its-name-as-a-string
+
+        //Log the Initial Object Properties
+        console.log(property,elementObj.style[property]);
+
+        //Use the Window Object to Call the 'Object.style.property' Method
+        window['elementHTML']['style'][property.toString()] = elementObj.style[property];
+
+    } //End of forIn Loop
+
 
     //Return the Completed HTML Object
     return elementHTML;
@@ -72,13 +106,13 @@ function buildHTML(elementObj)
 function changeColor(elementObj,color)
 {
     //Set JS Obj's backgroundColor Attribute to the Provided Color
-    elementObj.attributes.backgroundColor = color;
+    elementObj.style.backgroundColor = color.toString();
 
     //Get the HTML Document Element with ID matching JS Object's ID
     let elementHTML = document.getElementById(elementObj.attributes.id);
 
-    //Set the attribute of the HTML element to reflect Updated JS Object
-    elementHTML.setAttribute(background-color,elementObj.attributes.backgroundColor)
+    //Set the style property of the HTML element to reflect updated JS Object
+    elementHTML.style.backgroundColor = elementObj.style.backgroundColor;
 
 } //End of function 'changeColor'
 
